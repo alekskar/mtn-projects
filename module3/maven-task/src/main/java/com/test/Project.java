872cc557ab3672463d2443526a5fbef57921e115
alekskar <aliaksandr_karavai@epam.com>
2016-08-08 17:00:32 +0300

    Update Project.java

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -1,12 +1,16 @@
 package com.test;
 
 //dependency on Apache commons-collections
+//dependency on Apache commons-collections
+//dependency on Apache commons-collections
 import org.apache.commons.collections.Predicate;
 
 class Project {
 
   public static void test() {
     System.out.println("test");
+    System.out.println("test");
+    System.out.println("this is ci-work");
   }
   
   public static void main(String[] args) {
